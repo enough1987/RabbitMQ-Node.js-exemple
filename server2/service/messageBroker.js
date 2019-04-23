a5c47d969a8e9fd5e4953f97f3b3ddfe475ddc59
Oleksandr Tilha <oleksandr_tilha@epcnszxw0332.princeton.epam.com>
2019-04-23 14:46:13 +0800

    Fair dispatch

--- server2/service/messageBroker.js
+++ server2/service/messageBroker.js
@@ -8,13 +8,17 @@ const resive = () => {
   return open.then(function(conn) {
       return conn.createChannel();
     }).then(function(ch) {
-      return ch.assertQueue(q, {durable: true}).then(function(ok) {
-        return ch.consume(q, function(msg) {
-          if (msg !== null) {
-            console.log(JSON.parse(msg.content));
-            ch.ack(msg);
-          }
-        }, {noAck: false});
+      return ch.assertQueue(q, {durable: true})
+        .then(function(ok) {
+
+          ch.prefetch(1);   // <-- only get 1 message at a time
+          
+          return ch.consume(q, function(msg) {
+            if (msg !== null) {
+              console.log(JSON.parse(msg.content));
+              ch.ack(msg);
+            }
+          }, {noAck: false});
       });
     }).catch(console.warn);
 }
